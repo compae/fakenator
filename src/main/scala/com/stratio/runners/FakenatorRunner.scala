baad9e318951be77316b1a7168b20ce7d0586e05
Jose Pablo Fernandez <josepablofernandez@stratio.com>
2016-01-13 16:48:54 +0100

    changed for testing new decision sharding feature

--- src/main/scala/com/stratio/runners/FakenatorRunner.scala
+++ src/main/scala/com/stratio/runners/FakenatorRunner.scala
@@ -13,20 +13,26 @@ object FakenatorRunner {
 
   val stratioStreamingAPI = StratioStreamingAPIFactory
     .create
-    .withServerConfig("172.17.0.5",9092, "172.17.0.4", 2181)
+    .withServerConfig("localhost",9092, "localhost", 2181)
     .init
 
-  val streamName = "chustas"
+  val streamName = "drools_poc_stream"
 
   println(streamName)
 
   def main(args: Array[String]): Unit = {
+    /*
     if(args.size == 0) {
       println(s">> Creating stream: $streamName")
       createStream
     }
+*/
 
-    insertData
+     testCreateStream
+
+
+    //insertData
+    //insertDataTopic("topic_A")
   }
 
   def createStream(): Unit = {
@@ -49,15 +55,86 @@ object FakenatorRunner {
 
 
 
+  def testCreateStream():Unit = {
+
+    val data1 = new ColumnNameType("col1", ColumnType.INTEGER)
+    val data2 = new ColumnNameType("col2", ColumnType.STRING)
+
+    val columnList = Seq( data1, data2)
+
+    try {
+      stratioStreamingAPI.createStream(streamName, columnList)
+
+      UUID.randomUUID()
+    } catch {
+      case ssEx: StratioStreamingException => println(ssEx.printStackTrace())
+    }
+
+
+  }
+
+
+  def insertDataTopic(topicName:String): Unit = {
+
+    //var firstColumnValue = new ColumnNameValue("name", "test")
+    //var secondColumnValue = new ColumnNameValue("data1", new Integer(1))
+    //var thirdColumnValue = new ColumnNameValue("data2", new Double(2))
+
+    var firstColumnValue = new ColumnNameValue("col1",  new Integer(1))
+    var secondColumnValue = new ColumnNameValue("col2", "test_value")
+
+    var streamData = Seq(firstColumnValue, secondColumnValue)
+
+    try {
+      var total: Integer = 10000
+      var i : Integer = 0
+
+      while(i<total) {
+
+        println(s">> Inserting element: $i")
+
+        firstColumnValue = new ColumnNameValue("col1", i)
+        secondColumnValue = new ColumnNameValue("col2", s"test-$i")
+        streamData = Seq(firstColumnValue, secondColumnValue)
+
+        if (topicName!=null){
+           stratioStreamingAPI.insertData(streamName, streamData, topicName)
+        } else {
+          stratioStreamingAPI.insertData(streamName, streamData)
+        }
+
+
+        if(i % 10 == 0) {
+          Thread.sleep(250)
+        }
+
+
+        i = i + 1
+
+      }
+    } catch {
+      case ssEx: StratioStreamingException => println(ssEx.printStackTrace())
+    }
+  }
+
   def insertData(): Unit = {
-    var firstColumnValue = new ColumnNameValue("name", "test")
-    var secondColumnValue = new ColumnNameValue("data1", new Integer(1))
-    var thirdColumnValue = new ColumnNameValue("data2", new Double(2))
-    var streamData = Seq(firstColumnValue, secondColumnValue, thirdColumnValue)
+
+    //var firstColumnValue = new ColumnNameValue("name", "test")
+    //var secondColumnValue = new ColumnNameValue("data1", new Integer(1))
+    //var thirdColumnValue = new ColumnNameValue("data2", new Double(2))
+
+    var firstColumnValue = new ColumnNameValue("col1",  new Integer(1))
+    var secondColumnValue = new ColumnNameValue("col2", "test_value")
+
+    var streamData = Seq(firstColumnValue, secondColumnValue)
 
     try {
       var i: Integer = 1
 
+     // stratioStreamingAPI.listenStream(streamName)
+      stratioStreamingAPI.insertData(streamName, streamData)
+
+      /*
       while(true) {
         println(s">> Inserting element: $i")
         firstColumnValue = new ColumnNameValue("name", s"test-$i")
@@ -73,7 +150,7 @@ object FakenatorRunner {
         } else {
           i = i + 1
         }
-      }
+      }*/
     } catch {
       case ssEx: StratioStreamingException => println(ssEx.printStackTrace())
     }
